8371a8de984e5b922e4ac36cedaa7d544242e844
Meow42 <meow42@qq.com>
2024-11-03 18:57:42 +0800

    存储位置变更，路径调整

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tenvi/app/avatar/',
+  base: '/app/avatar/',
   plugins: [
     vue()
   ]
